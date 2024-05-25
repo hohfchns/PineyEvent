90ce1d90e8eca73700fa527d51d0025be346dbc8
Sahar Shulman <saharshulman65@gmail.com>
2024-05-25 22:21:24 +0300

    Added strongly-typed return types

--- src/piney_event/event.py
+++ src/piney_event/event.py
@@ -2,13 +2,13 @@ import weakref
 import inspect
 
 class Event:
-    def __init__(self):
+    def __init__(self) -> None:
         self.receivers: list = []
         self.ignore_error = True
         # Alias `disconnect` to `erase`
         self.disconnect = self.erase
     
-    def connect(self, callback):
+    def connect(self, callback) -> None:
         """
         :param callback: - A callable that will be called with parameters when event is emitted
         """
@@ -20,7 +20,7 @@ class Event:
         else:
             self.receivers.append(weakref.ref(callback))
     
-    def erase(self, callback):
+    def erase(self, callback) -> None:
         """
         Removes the connection to `callback`
         :param callback: - The callable that will be erased
@@ -28,13 +28,13 @@ class Event:
 
         self.receivers.remove(callback)
     
-    def clear(self):
+    def clear(self) -> None:
         """
         Erase all callbacks
         """
         self.receivers.clear()
     
-    def emit(self, *args):
+    def emit(self, *args) -> None:
         """
         :param args: - arguments to be emitted
         """
@@ -57,18 +57,18 @@ class TypedEvent(Event):
     """
     Subclass of `Event` with strong-typed parameters
     """
-    def __init__(self, *param_types):
+    def __init__(self, *param_types) -> None:
         super().__init__()
         self.param_types = param_types
 
-    def emit(self, *args):
+    def emit(self, *args) -> None:
         emit_types = tuple([type(param) for param in args])
         if emit_types != self.param_types:
             raise TypeError(f"TypedEvent emit expected argument types '{self.param_types}', but got `{emit_types} instead.`")
 
-        return super().emit(*args)
+        super().emit(*args)
     
-    def connect(self, callback):
+    def connect(self, callback) -> None:
         callback_args = inspect.signature(callback).parameters
         l = len(callback_args)
         for p in callback_args:
@@ -78,7 +78,7 @@ class TypedEvent(Event):
         if l != len(self.param_types):
             raise TypeError(f"TypedEvent connect expected argument count of {len(self.param_types)}, but target has {l}. Target params: `{callback.__code__.co_varnames}`")
 
-        return super().connect(callback)
+        super().connect(callback)
 
 if __name__ == "__main__":
     class TestObj():
