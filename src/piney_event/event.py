bb827cab353bc04e4d981d74f7ec62d97f10b23d
Sahar Shulman <saharshulman65@gmail.com>
2024-05-25 22:54:09 +0300

    Added connection flags, CONNECT_ONE_SHOT flag, stronger typing, and better docstrings

--- src/piney_event/event.py
+++ src/piney_event/event.py
@@ -1,32 +1,46 @@
 import weakref
 import inspect
+from typing import Callable, List, NamedTuple
 
 class Event:
+    class ConnectFlags:
+        CONNECT_ONE_SHOT: int = (1 >> 0)
+
+    class ConnectionType(NamedTuple):
+        callback: Callable
+        flags: int
+
     def __init__(self) -> None:
-        self.receivers: list = []
+        self.receivers: List[Event.ConnectionType] = []
         self.ignore_error = True
         # Alias `disconnect` to `erase`
         self.disconnect = self.erase
     
-    def connect(self, callback) -> None:
+    def connect(self, callback: Callable, flags: int = 0x0) -> None:
         """
-        :param callback: - A callable that will be called with parameters when event is emitted
+        :param callback: A callable that will be called with parameters when event is emitted
+        :param flags: An integer bitmask of `Event.ConnectFlags`. For multiple flags, use the `|` operator on all desired values.
+        :flag CONNECT_ONE_SHOT: Will disconnect the callback upon the first emit.
         """
         if not callable(callback):
             raise TypeError("Tried to connect non-callable to event!")
 
         if inspect.ismethod(callback):
-            self.receivers.append(weakref.WeakMethod(callback))
+            self.receivers.append(Event.ConnectionType(weakref.WeakMethod(callback), flags))
         else:
-            self.receivers.append(weakref.ref(callback))
+            self.receivers.append(Event.ConnectionType(weakref.ref(callback), flags))
     
-    def erase(self, callback) -> None:
+    def erase(self, callback: Callable) -> None:
         """
         Removes the connection to `callback`
-        :param callback: - The callable that will be erased
+        :param callback: The callable that will be erased
         """
 
-        self.receivers.remove(callback)
+        for i in range(len(self.receivers)):
+            if self.receivers[i].callback() == callback:
+                del self.receivers[i]
+                i -= 1
+                continue
     
     def clear(self) -> None:
         """
@@ -36,10 +50,10 @@ class Event:
     
     def emit(self, *args) -> None:
         """
-        :param args: - arguments to be emitted
+        :param args: arguments to be emitted
         """
         for i in range(len(self.receivers)):
-            callback = self.receivers[i]()
+            callback = self.receivers[i].callback()
             if callback is None:
                 del self.receivers[i]
                 i -= 1
@@ -52,6 +66,12 @@ class Event:
                         pass
             else:
                 callback(*args)
+                
+            if self.receivers[i].flags & Event.ConnectFlags.CONNECT_ONE_SHOT:
+                del self.receivers[i]
+                i -= 1
+                continue
+
 
 class TypedEvent(Event):
     """
@@ -68,7 +88,7 @@ class TypedEvent(Event):
 
         super().emit(*args)
     
-    def connect(self, callback) -> None:
+    def connect(self, callback: Callable, flags: int = 0x0) -> None:
         callback_args = inspect.signature(callback).parameters
         l = len(callback_args)
         for p in callback_args:
@@ -78,15 +98,32 @@ class TypedEvent(Event):
         if l != len(self.param_types):
             raise TypeError(f"TypedEvent connect expected argument count of {len(self.param_types)}, but target has {l}. Target params: `{callback.__code__.co_varnames}`")
 
-        super().connect(callback)
+        super().connect(callback, flags)
 
 if __name__ == "__main__":
     class TestObj():
         def test_cb(self, s: str):
             print(s)
+
+    def test_global_cb(s: str):
+        print(s)
     
     e = TypedEvent(str)
     to = TestObj()
+    e.connect(to.test_cb, Event.ConnectFlags.CONNECT_ONE_SHOT)
+    e.emit("This is working.")
+    e.emit("This is not working.")
     e.connect(to.test_cb)
     e.emit("This is working.")
+    e.emit("This is working.")
+    e.disconnect(to.test_cb)
+    e.emit("This is not working.")
 
+    e.connect(test_global_cb, Event.ConnectFlags.CONNECT_ONE_SHOT)
+    e.emit("This is working.")
+    e.emit("This is not working.")
+    e.connect(test_global_cb)
+    e.emit("This is working.")
+    e.emit("This is working.")
+    e.disconnect(test_global_cb)
+    e.emit("This is not working.")
